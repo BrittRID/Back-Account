b77c7d8687ff9e1d01514a9ea67f9447027f7e20
Brittany Ridley <britt.ridley@outlook.com>
2021-10-04 20:44:06 -0500

    BankAccount

---
+++ BankEncapsulationExercise/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulationExercise
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Get user deposit of {amount} to your account!!");
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+
+        }
+    }
+}
---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount be = new BankAccount();
+            Console.WriteLine("How much money do you need to deposit?");
+            double amountToDeposit = double.Parse(Console.ReadLine());
+
+            be.Deposit(amountToDeposit);
+            double userBalance = be.GetBalance();
+
+            Console.WriteLine($"Balance available{userBalance, 0:c}");
+        }
+    }
+}
